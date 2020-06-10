ae467df929297ebc67259581d3e63a45d91ab31b
Marissa <marissacooterl@gmail.com>
2020-06-09 19:49:04 -0500

    post router and custom middleware built out

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -17,7 +17,7 @@ router.get('/', (req, res) => {
   })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validatePostId, (req, res) => {
   const id = req.params.id
   posts
     .getById(id)
@@ -55,13 +55,48 @@ router.delete('/:id', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+  if (!req.body) {
+    return res.status(400).json({
+      message: "Missing data."
+    })
+  }
+  posts
+    .update(id, changes)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post)
+      } else {
+        res.status(404).json({
+          message: "The post could not be found."
+        })
+      }
+    })
+    .catch((error) => {
+      console.log(error)
+      res.status(500).json({
+        message: "Error updating the user."
+      })
+    })
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  const id = req.params.id
+  posts
+    .getById(id)
+    .then((post) => {
+      if (post) {
+        req.post = post
+        console.log(post)
+        next()
+      } else {
+        res.status(404).json({
+          message: "Post not found."
+        })
+      }
+    })
+    .catch(next)
 }
 
 module.exports = router;
