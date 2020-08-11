afbe9fd902f743301c19eb7ea15d830446b16b97
Marissa <marissacooterl@gmail.com>
2020-08-11 13:58:25 -0500

    logger done and moved into it's own file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const server = require("./server")
 const express = require("express")
+const server = require("./server")
+const logger = require("./middleware/logger")
 
 server.use(express.json())
-
+server.use(logger())
--- server.js
+++ server.js
@@ -5,11 +5,7 @@ const port = 4000
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
-});
-
-//custom middleware
-
-function logger(req, res, next) {}
+})
 
 server.listen(port, () => {
   console.log(`[ Server running at http://localhost${port} ... ]`)
