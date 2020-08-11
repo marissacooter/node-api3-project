616177365df9955149d7d727c4985d6b87abcd35
Marissa <marissacooterl@gmail.com>
2020-08-11 14:52:39 -0500

    get users not working, moving on to get by id for now

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,21 +1,49 @@
-const express = require('express');
+const express = require('express')
+const users = require("./userDb")
 
-const router = express.Router();
+const router = express.Router()
 
 router.post('/', (req, res) => {
-  // do your magic!
-});
+  
+})
 
 router.post('/:id/posts', (req, res) => {
   // do your magic!
-});
+})
 
-router.get('/', (req, res) => {
-  // do your magic!
-});
+router.get('/users', (req, res) => {
+  users.get()
+  .then((data) => {
+    res.status(200).json(data)
+  })
+  .catch((err) => {
+    console.log(err)
+    res.status(500).json({
+      message: "Error retrieving users"
+    })
+  })
+})
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  const id = req.params.id
+
+  users.getById(id)
+  .then((user) => {
+    if (user) {
+      res.status(200).json(user)
+    } else {
+      res.status(400).json({
+        message: "User could not be found"
+      })
+    }
+  })
+  .catch((err) => {
+    console.log(err)
+    res.status(500).json({
+      message: "Error retrieving user"
+    })
+  })
+
 });
 
 router.get('/:id/posts', (req, res) => {
@@ -29,6 +57,9 @@ router.delete('/:id', (req, res) => {
 router.put('/:id', (req, res) => {
   // do your magic!
 });
+
+
+
 
 //custom middleware
 
